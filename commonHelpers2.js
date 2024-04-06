45a0d0f395dcc8d6ff4349977f0f78795ecdc234
RomanFS76 <36919661+RomanFS76@users.noreply.github.com>
2024-04-06 06:00:36 +0000

    Deploying to gh-pages from @ RomanFS76/goit-js-hw-10@6924fae3a7b0d1475d2c339aed3a31ef12dd9f78 🚀

---
+++ commonHelpers2.js
@@ -0,0 +1,2 @@
+import"./assets/modulepreload-polyfill-3cfb730f.js";const o=document.querySelector(".form"),t=document.querySelector('input[name="delay"]');console.log(t);document.querySelector('button[type="submit"]');o.addEventListener("click",e=>{e.preventDefault(),console.log(e)});
+//# sourceMappingURL=commonHelpers2.js.map
