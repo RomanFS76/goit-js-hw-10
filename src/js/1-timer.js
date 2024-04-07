2b5ca73610ccdd372a64f520a05992ac58720e60
RR <korvy85@gmail.com>
2024-04-07 19:42:53 +0300

    bad timer

--- src/js/1-timer.js
+++ src/js/1-timer.js
@@ -1,7 +1,8 @@
 import flatpickr from 'flatpickr';
 import 'flatpickr/dist/flatpickr.min.css';
 
-
+import iziToast from 'izitoast';
+import 'izitoast/dist/css/iziToast.min.css';
 
 const btnStart = document.querySelector('[data-start]');
 const daysEl = document.querySelector('[data-days]');
@@ -9,85 +10,90 @@ const hoursEl = document.querySelector('[data-hours]');
 const minutesEL = document.querySelector('[data-minutes]');
 const secondsEl = document.querySelector('[data-seconds]');
 
-btnStart.addEventListener('click', event => {
-    btnStart.disabled = false;
-    startTimer();
-
-});
-
 btnStart.disabled = true;
 
-const currentTime = new Date();
 let userSelectDates;
 
 const options = {
-    enableTime: true,
-    time_24hr: true,
-    defaultDate: new Date(),
-    minuteIncrement: 1,
-    
-    onClose(selectedDates) {
+  enableTime: true,
+  time_24hr: true,
+  defaultDate: new Date(),
+  minuteIncrement: 1,
+
+  onClose(selectedDates) {
+    if (selectedDates[0] >= new Date()) {
+      btnStart.disabled = false;
         userSelectDates = selectedDates[0];
-        if (userSelectDates >= currentTime) {
-            btnStart.disabled = false;
-        } else {
-            btnStart.disabled = true;
-        window.alert('Please choose a date in the future');
-        }
+    } else {
+      btnStart.disabled = true;
+      iziToast.show({
+        message: 'Please choose a date in the future',
+        position: 'topCenter',
+        color: 'yellow',
+        timeout: 1500,
+      });
     }
+  },
 };
 
 flatpickr('#datetime-picker', options);
 
-function startTimer() {
-    const intervalId = setInterval(timer, 1000);
-    // clearInterval(intervalId);
-}
+btnStart.addEventListener('click', event => {
+    let diff = userSelectDates - Date.now();
+    const convertDate = convertMs(diff);
 
-const differenceTime = userSelectDates - currentTime;
+    updateTimer(convertDate);
 
-
-console.log(`currentTime: ${currentTime}`);
-console.log(`differenceTime: ${differenceTime}`);
-console.log(`userSelectDates: ${userSelectDates}`);
-
-
-function timer() {
-    if (differenceTime > 0) {
-        differenceTime -=1000;
-        updateTimer(convertMs(differenceTime));
-    } else {
-        clearInterval(intervalId);
+    function startTimer() {
+        const intervalId = setInterval(() => {
+        if (diff > 2) {
+            diff = diff - 1000;
+            updateTimer(convertMs(diff));
+            console.log(intervalId);
+        } else {
+            clearInterval(intervalId);
+        }
+        }, 1000);
     }
-}
 
-function updateTimer({ days, hours, minutes, seconds }) { 
-    daysEl.textContent = `${days}`;
-    hoursEl.textContent = `${hours}`;
-    minutesEL.textContent = `${minutes}`;
-    secondsEl.textContent = `${seconds}`;
-};
-
-function addLeadingZero(value) {
-    return String(value).padStart(2, '0');
-}
+    startTimer();
+    btnStart.disabled = true;
+});
 
 function convertMs(ms) {
-    // Number of milliseconds per unit of time
-    const second = 1000;
-    const minute = second * 60;
-    const hour = minute * 60;
-    const day = hour * 24;
+  // Number of milliseconds per unit of time
+  const second = 1000;
+  const minute = second * 60;
+  const hour = minute * 60;
+  const day = hour * 24;
 
-    // Remaining days
-    const days = addLeadingZero(Math.floor(ms / day));
-    // Remaining hours
-    const hours = addLeadingZero(Math.floor((ms % day) / hour));
-    // Remaining minutes
-    const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
-    // Remaining seconds
-    const seconds = addLeadingZero(
-        Math.floor((((ms % day) % hour) % minute) / second)
-    );
+  // Remaining days
+  const days = addLeadingZero(Math.floor(ms / day));
+  // Remaining hours
+  const hours = addLeadingZero(Math.floor((ms % day) / hour));
+  // Remaining minutes
+  const minutes = addLeadingZero(Math.floor(((ms % day) % hour) / minute));
+  // Remaining seconds
+  const seconds = addLeadingZero(
+    Math.floor((((ms % day) % hour) % minute) / second)
+  );
+
+  return { days, hours, minutes, seconds };
 }
+
+function updateTimer({ days, hours, minutes, seconds }) {
+  daysEl.textContent = `${days}`;
+  hoursEl.textContent = `${hours}`;
+  minutesEL.textContent = `${minutes}`;
+  secondsEl.textContent = `${seconds}`;
+}
+
+function addLeadingZero(value) {
+  return String(value).padStart(2, '0');
+}
+
+
+
+
+
 
